aeccf31a8dc575c31b140ef3d3b46d69e6d1150e
tycompton <tyrone.compton@gmail.com>
2021-01-29 16:00:15 +0000

    ejs views and partials added

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for(let i = 0; i < 50; i++){
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere consequuntur voluptatibus ratione eligendi officia, explicabo voluptate, aut accusamus nam atque, quos expedita sunt impedit iusto nulla dolorem quod ex sed?",
+      price
     })
     await camp.save();
   }
