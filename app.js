ba4580ca630e2d8b8aaeab426dca89a30675ab1b
tycompton <tyrone.compton@gmail.com>
2021-02-03 14:45:32 +0000

    browser validations implemented

--- app.js
+++ app.js
@@ -41,12 +41,15 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`)
-  // res.send(req.body);
-})
+app.post("/campgrounds", async (req, res, next) => {
+  try {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
@@ -69,6 +72,12 @@ app.delete('/campgrounds/:id', async (req, res) => {
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
 })
+
+
+app.use((err, req, res, next) => {
+  res.send('Something went wrong!')
+})
+
 
 
 app.listen(3000, () => {
